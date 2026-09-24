8bbb35710148377ac95b2c1f2c1a6f87975f88be
Josué Torres <josuejesusts@gmail.com>
2026-09-24 12:12:42 -0500

    chore: clean unused native dependencies and set fallback PORT for production

--- config/env.js
+++ config/env.js
@@ -2,9 +2,10 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
+export const PORT = process.env.PORT || 5500;
+export const NODE_ENV = process.env.NODE_ENV || "development";
+
 export const {
-  PORT,
-  NODE_ENV,
   DB_URI,
   JWT_SECRET,
   JWT_EXPIRES_IN,
